16ed35598eb7fe7d0e97e92f2c5a1f621279b1ec
Liam Cammpbell <liam22campbell@gmail.com>
2022-06-23 16:03:53 -0700

    Pass second second test.
    Co-Authored-By: Ashe Urban<Ashe@goldentongue.com>
    Co-Authored-By: PJ Perry <pjperry28@gmail.com>
    Co-Authored-By: Ashe Urban<ashe@goldentongue.com>

--- __tests__/biz-func.test.js
+++ __tests__/biz-func.test.js
@@ -4,11 +4,23 @@ import Character from '../src/js/biz-func.js';
 describe("Character", () => {
 
   test("should create a character object with multiple properties", () => {
-    const character = new Character(2,4,5);
-    expect(character.strength).toEqual(2);
-    expect(character.knowledge).toEqual(4);
-    expect(character.command).toEqual(5); 
+    const character = new Character();
+    expect(character.xP).toEqual();
+    expect(character.knowledge).toEqual();
+    expect(character.command).toEqual(); 
+    expect(character.strength).toEqual();
   });
+
+  test("should add 1 to strength when 10xP is reached", () => {
+    const character = new Character();
+    expect(character.AddStats()).toEqual();
+  });
+});
+
+  // test("should add 1 to strength when 10xP is reached", () => {
+  //   const character = new Character(0);
+  //   expect(character.AddStats()).toEqual();
+  // });
 
   // test('should correctly determine whether three lengths are not a Character', () => {
   //   const notCharacter = new Character(3,9,22);
@@ -17,6 +29,5 @@ describe("Character", () => {
 
 
    
-  });
-
+  
 
--- src/js/biz-func.js
+++ src/js/biz-func.js
@@ -1,16 +1,42 @@
 import $ from 'jquery';
 
-class Character {
-   constructor(strength,knowledge,command) {
-     this.strength = strength ;
-     this.knowledge = knowledge;
-     this.command = command;
-     
-   }
+export default function Character(xP, strength, knowledge, command) {
+  this.xP = xP;
+  this.strength = strength;
+  this.knowledge = knowledge;
+  this.command = command;
+  }
 
-  // return 'hi there, from the deck';
-};
+  let newCharacter1 = new Character();
+  
 
-export default Character;
+  Character.prototype.AddStats = function () {
+    if (this.xP = 10) {
+      this.strength = 1, this.knowledge = 1, this.command = 1;
+    } 
+  }
+
+  // Character.prototype.LevelUp = function () {
+
+  // }
+  //  class Stats extends Character {
+  //   constructor(strength) { // add back properties as needed
+  //      this.strength = strength;
+  //     // this.knowledge = knowledge;
+  //     // this.command = command;
+  //  }
+   
+
+  //     levelUp() {
+  //     if (this.xP = 10) {
+  //       this.strength += 1;
+  //       // this.knowledge += 1;
+  //       // this.command += 1;
+  //       this.xP = 0;
+  //     } else {
+  //     } 
+  //     return Stats
+  //   }
+  // }
 
 
